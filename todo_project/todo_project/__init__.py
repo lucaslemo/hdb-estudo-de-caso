06aeaeb6edef90871e1553320c2c507266b6d217
lucaslemo <lucaslemo852@gmail.com>
2024-09-10 13:35:40 -0300

    feat: base do projeto para deploy

---
+++ todo_project/todo_project/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['ENV'] = os.getenv('FLASK_ENV', 'development')
+app.config['DEBUG'] = os.getenv('FLASK_DEBUG', True)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' 
+login_manager.login_message_category = 'danger'
+
+bcrypt = Bcrypt(app)
+
+# Always put Routes at end
+from todo_project import routes
